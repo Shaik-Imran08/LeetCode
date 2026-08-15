8336a8d3ed3f7dd31b34708f4d4d2d0d89805cc8
Imran Shaik <shaikkimrran@gmail.com>
2026-08-15 14:58:35 +0530

    Time: 15 ms (40.56%) | Memory: 23.8 MB (25.74%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,16 @@
+import collections
+from typing import List
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        ans = collections.defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26
+
+            for char in s:
+                count[ord(char) - ord("a")] += 1
+            ans[tuple(count)].append(s)
+        return list(ans.values())
